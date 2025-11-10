414d6d50b58e666f80c0bdc0e45c3f4f796a0f31
Christophe REVOIRE <christophe.revoire@tagginfo.com>
2025-11-10 15:07:13 +0100

    Ajout du suivi de serveurs au démarrage

--- bot.js
+++ bot.js
@@ -71,6 +71,9 @@ client.once('ready', () => {
     console.log(`${client.user.tag} est connecté et prêt !`);
     console.log(`ID du bot: ${client.user.id}`);
     console.log('------');
+    console.log(`/reglement enregistré sur`);
+    console.log(`Actif sur ${client.guilds.cache.size} serveur(s)`);
+    console.log('------');
     console.log(`Attribution de rôle: ${VERIFIED_ROLE_ID !== '0' ? '✅ Activée' : '❌ Désactivée'}`);
     console.log(`Logs Discord: ${LOG_CHANNEL_ID !== '0' ? '✅ Activés' : '❌ Désactivés'}`);
     console.log('------');
